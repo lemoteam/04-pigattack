f986433f92faa44180d1ddda6f620efb43419e4a
johnfordman <ndoulou.jean@gmail.com>
2018-05-22 11:24:11 +0200

    refactor sldier controller with min distance

--- Assets/sliderController.cs
+++ Assets/sliderController.cs
@@ -1,13 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class sliderController : MonoBehaviour {
 	private GameObject indicator;
 	private GameObject indicatorS;
 	private GameObject[] indexes;
-	//private GameObject[] checkpoint;
-	//private float checkpointL;
 	private float posIndex;
 	public PlayerManager playerManager;
 	private float previousIndex = -1;
@@ -23,50 +19,28 @@ public class sliderController : MonoBehaviour {
 	void Update ()
 	{
 		if (indicator && indicatorS.GetComponent<MeshRenderer>().enabled )
-		{	
+		{
+			var minimum = float.MaxValue;
 			for (int i = 0; i < indexes.Length; i++)
 			{
-				var dist = indexes[i].transform.position.x - indicator.transform.position.x;
-				if (dist < 2 && dist > 0)
+				var num = Vector3.Distance(indexes[i].transform.position, indicator.transform.position);
+				if (num < minimum)
 				{
+					minimum = num;	
+					// Debug.Log("<color=black> min distance : "+ minimum +"</color>");
 					var currentIndex = i;
 					if (previousIndex != currentIndex)
 					{
 						playerManager.goToCheckpoint(currentIndex);
 						previousIndex = currentIndex;
 					}
-					//DEBUG
-					setIndicColor(indexes[i],Color.blue, Color.blue);
-				} else {
-					setIndicColor(indexes[i],Color.white, Color.white);
 				}
 			}
-			Debug.Log("<color=yellow> distance indic 1 : "+ (indexes[1].transform.position.x - indicator.transform.position.x) +"</color>");
-			Debug.Log("<color=white> distance indic 2: " + (indexes[2].transform.position.x - indicator.transform.position.x) + "</color>");
-			Debug.Log("<color=red> distance indic 3 : "+(indexes[0].transform.position.x - indicator.transform.position.x) +"</color>");
+			/*
+			Debug.Log("<color=white> distance indic 1 : "+ Vector3.Distance(indexes[0].transform.position,indicator.transform.position) +"</color>");
+			Debug.Log("<color=red> distance indic 2 : "+ Vector3.Distance(indexes[1].transform.position,indicator.transform.position) +"</color>");
+			Debug.Log("<color=yellow> distance indic 3 : "+ Vector3.Distance(indexes[2].transform.position,indicator.transform.position) +"</color>");
+			*/
 		}
-	}
-	
-	public float Map(float from, float to, float from2, float to2, float value) {
-		if(value <= from2) {
-			return from;
-		} else if(value >= to2) {
-			return to;
-		} else {
-			return (to - from) * ((value - from2) / (to2 - from2)) + from;
-		}
-	}
-	private void setIndicColor(GameObject indic, Color color, Color specular)
-	{
-		//Fetch the Renderer from the GameObject
-		Renderer rend = indic.GetComponent<Renderer>();
-
-		//Set the main Color of the Material to green
-		rend.material.shader = Shader.Find("_Color");
-		rend.material.SetColor("_Color", color);
-
-		//Find the Specular shader and change its Color to red
-		rend.material.shader = Shader.Find("Specular");
-		rend.material.SetColor("_SpecColor", specular);
 	}
 }
